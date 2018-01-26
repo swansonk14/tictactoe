aebf8c558fa782a065254ae720228a184bca2cb3
Kyle Swanson <swansonk.14@gmail.com>
2018-01-27 01:14:54 +0600

    Click handlers for adding x and o

--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,42 @@
 var board = [['', '', ''], ['', '', ''], ['', '', '']];
+var turn = 'x';
 
-$(document).ready(function() {
-    for (var i = 0; i <= 2; i++) {
-        for (var j = 0; j <= 2; j++) {
-            console.log("board-" + i + "-" + j);
-            var box = document.getElementById("board-" + i + "-" + j);
-            console.log(box);
+function setTurnText() {
+    $('#turn')[0].innerHTML = 'Player ' + turn + '\'s turn';
+}
+
+function changeTurns() {
+    turn = (turn == 'x') ? 'o' : 'x';
+    setTurnText();
+}
+
+function restartGame() {
+    turn = 'x';
+    setTurnText();
+
+    for (var row = 0; row <= 2; row++) {
+        for (var col = 0; col <= 2; col++) {
+            $('boar-' + row + '-' + col)[0].innerHTML = '';
         }
     }
+}
+
+$(document).ready(function() {
+    $('#restart').click(restartGame);
+
+    // Set up click handlers for boxes
+    for (var row = 0; row <= 2; row++) {
+        for (var col = 0; col <= 2; col++) {
+            $('#board-' + row + '-' + col).click(function () {
+                var box = $(this)[0];
+
+                if (box.innerHTML == '') {
+                    box.innerHTML = '<img src="images/' + turn + '.png" \>';
+                    changeTurns();
+                }
+            });
+        }
+    }
+
+    restartGame();
 });
